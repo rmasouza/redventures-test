01e9894903f276e0a811c8ad596e76a36fbdf0c0
Romulo Messias <romulomessias@me.com>
2019-09-07 12:09:45 -0300

    added next button to go to next page  in SimulationResume component

--- src/components/common/simulation/SimulationResume.tsx
+++ src/components/common/simulation/SimulationResume.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './SimulationResume.scss';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '../../../stores/StoreProvider';
+import ButtonLink from '../ButtonLink';
+import { withRouter, RouteProps } from 'react-router';
+import ArrowIcon from '../icons/ArrowIcon';
 
-const SimulationResumeBase = () => {
+
+const SimulationResumeBase: FC<RouteProps> = (props: RouteProps) => {
+    const { location } = props;
     const store = useStore();
-    
+
+
+    const steps: Record<string, string> = {
+        '/engines': '/colors',
+        '/colors': '/wheels',
+        '/wheels': '/resume',
+    }
+
+    const currentPath: string = location ? location.pathname : '/';
+
     return (
         <footer className='simulation-resume'>
             <span>{store.finalPrice}</span>
@@ -13,22 +27,20 @@ const SimulationResumeBase = () => {
             <span>Model R</span>
 
             <span>
-            {
-                store.currentEngine && `${store.currentEngine.kwh} ${store.currentEngine.type}`
-            }
-            </span>
-
-            
-            <span>
-
+                {
+                    store.currentEngine && `${store.currentEngine.kwh} ${store.currentEngine.type}`
+                }
             </span>
             <span>
-
+            </span>
+            <span>
+                <ButtonLink text={'next'} to={steps[currentPath]} icon={<ArrowIcon />} />
             </span>
         </footer>
     )
 }
 
-const SimulationResume = observer(SimulationResumeBase)
+//@ts-ignore
+const SimulationResume = withRouter(observer(SimulationResumeBase))
 
 export default SimulationResume;
